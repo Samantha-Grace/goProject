83895a19908b91f9fb594b9fdbdce314fe7ec8e8
Samantha-Grace <sgrace732@gmail.com>
2021-05-06 14:41:37 -0600

    games with go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type position struct {
+	x float32
+	y float32
+}
+
+type badGuy struct {
+	name   string
+	health int
+	pos    position
+}
+
+func whereIsBadGuy(b badGuy) {
+	x := b.pos.x
+	y := b.pos.y
+	fmt.Println("(", x, ",", y, ")")
+}
+
+func main() {
+
+	p := position{4, 2}
+
+	fmt.Println(p.x)
+
+	badguy := badGuy{"Susan the kitten", 100, p}
+	whereIsBadGuy(badguy)
+}
